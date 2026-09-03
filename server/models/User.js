e10cddbf4d32bb5d7560ab8adf3e1cedc8118a5c
turla <turladanell01@gmail.com>
2026-09-03 18:19:49 +0800

    added coma to fix the error

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: [true, 'Username is required'], unique: true },
     email: { type: String, required: [true, 'Email is required'], unique: true },
     password: { type: String, required: [true, 'Password is required'] },
-    isAdmin: { type: Boolean, default: false }
+    isAdmin: { type: Boolean, default: false },
 
     followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
